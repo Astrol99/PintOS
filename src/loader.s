9ef43132fb1804ea0fc31efaf5417443025505eb
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-06-29 14:57:46 -0500

    Setup stack for c in loader and called c code from assembly

--- src/loader.s
+++ src/loader.s
@@ -1,5 +1,7 @@
 global loader                   ; the entry symbol for ELF
 
+KERNEL_STACK_SIZE equ 4096      ; stack size in bytes
+
 MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
 FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
@@ -12,6 +14,11 @@ align 4                         ; the code must be 4 byte aligned
     dd CHECKSUM                 ; and the checksum
 
 loader:                         ; the loader label (defined as entry point in linker script)
-    mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
-.loop:
-    jmp .loop                   ; loop forever
+    external kmain              ; function kmain defined in kmain.c
+    call kmain                  ; call c code 
+
+section .bss:
+align 4
+kernel_stack:                   ; label points to beginning of memory
+    resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
+    mov esp, kernel_stack + KERNEL_STACK_SIZE       ; point esp to the start of the stack (end of memory area)
