6fd060e991abc4bd26d64a922f32f686da1dadb2
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-07-01 23:40:53 -0500

    Added strlen string util

---
+++ src/utils/string.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_STRING_H
+#define INCLUDE_STRING_H
+
+/** strlen:
+ *  Returns the length of string linearly incrementing
+ *  
+ *  @param str The string to get the length of
+ */
+int strlen(char *str);
+
+#endif /* INCLUDE_STRING_H */
