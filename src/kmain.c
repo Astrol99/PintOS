377a5c3a769704d2b19da0da638dfa46e20c4bc1
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-07-01 20:43:36 -0500

    Added framebuffer for text with color

--- src/kmain.c
+++ src/kmain.c
@@ -1,3 +1,25 @@
+#define FB_GREEN     2
+#define FB_DARK_GREY 8
+
+/** fb_write_cell:
+ *  Writes a character with the given foreground and background to position i
+ *  in the framebuffer.
+ *
+ *  @param i  The location in the framebuffer
+ *  @param c  The character
+ *  @param fg The foreground color
+ *  @param bg The background color
+ */
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
+{
+    char *fb = (char *)0x000B8000;  // Create frame buffer
+
+    fb[i] = c;  // Assign character to framebuffer
+    fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);   // Adjust background and foreground of text
+}
+
 int kmain() {
+    fb_write_cell(0, 'A', 0, 3);
+
     return 0;
 }
