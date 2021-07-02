918567a3aacf7d98325a795859799214540f8dd3
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-07-01 23:19:58 -0500

    Moved IO defines into io.h

--- src/drivers/output.c
+++ src/drivers/output.c
@@ -1,17 +1,6 @@
 #include "output.h"
 #include "io.h"
 
-#define FB_GREEN     2
-#define FB_DARK_GREY 8
-
-/* The I/O ports */
-#define FB_COMMAND_PORT         0x3D4
-#define FB_DATA_PORT            0x3D5
-
-/* The I/O port commands */
-#define FB_HIGH_BYTE_COMMAND    14
-#define FB_LOW_BYTE_COMMAND     15
-
 void fb_move_cursor(unsigned short pos)
 {
     outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
