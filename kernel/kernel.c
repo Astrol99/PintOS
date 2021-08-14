307dac338ceabc0501d659227827056f906e669c
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-08-14 02:13:04 -0500

    Added support for newlines in terminal driver

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,10 @@
 #include "../drivers/vga.h"
 
+/*  vscode intellisense trippin here
 #if defined(__linux__)
 #error "You are not using a cross-compiler, u suck"
 #endif
+*/
 
 #if !defined(__i386__)
 #error "OS needs to be compiled with ix86-elf compiler, dumbass"
@@ -12,5 +14,5 @@ void kmain(void)
 {
     terminal_initialize();
 
-    terminal_writestring("Hello!");
+    terminal_writestring("Hello\nWorld!");
 }
