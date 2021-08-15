546fda0e86df3253e91e826a4f2caac5714dc546
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-08-14 22:50:25 -0500

    Added more cursor options

--- drivers/tty.c
+++ drivers/tty.c
@@ -12,25 +12,37 @@ size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
- 
-void terminal_initialize(void)
-{
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;	// Get address of terminal buffer
-	// Loop through entire buffer and clear it
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
-		}
-	}
-}
- 
+
 void terminal_setcolor(uint8_t color) 
 {
 	terminal_color = color;
+}
+
+void terminal_enable_cursor(uint8_t cursor_start, uint8_t cursor_end)
+{
+	outb(TTY_COMMAND_PORT, 0x0A);
+	outb(TTY_DATA_PORT, (inb(TTY_DATA_PORT) & 0xC0) | cursor_start);
+
+	outb(TTY_COMMAND_PORT, 0x0B);
+	outb(TTY_DATA_PORT, (inb(TTY_DATA_PORT) & 0xC0) | cursor_end);
+}
+
+void terminal_disable_cursor(void)
+{
+	outb(TTY_COMMAND_PORT, 0x0A);
+	outb(TTY_DATA_PORT, 0x20);
+}
+
+uint16_t terminal_get_cursor_position(void)
+{
+	uint16_t pos = 0;
+	
+	outb(TTY_COMMAND_PORT, TTY_LOW_BYTE_COMMAND);
+	pos |= inb(TTY_DATA_PORT);
+	outb(TTY_COMMAND_PORT, TTY_HIGH_BYTE_COMMAND);
+	pos |= ((uint16_t)inb(TTY_DATA_PORT)) << 8;
+	
+	return pos;
 }
 
 void terminal_move_cursor(int x, int y)
@@ -43,6 +55,23 @@ void terminal_move_cursor(int x, int y)
 	outb(TTY_DATA_PORT, (uint8_t)((pos >> 8) & 0xFF));
 }
 
+
+void terminal_initialize(void)
+{
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;	// Get terminal buffer from address
+	//terminal_enable_cursor(0,14);
+	// Loop through entire buffer and clear it
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry(' ', terminal_color);
+		}
+	}
+}
+ 
 void terminal_scrolldown(void)
 {
 	terminal_row--;
--- drivers/tty.h
+++ drivers/tty.h
@@ -1,9 +1,16 @@
 #ifndef TTY_H
 #define TTY_H
  
-#include <stddef.h>
+ #include <stdint.h>
+ #include <stddef.h>
  
 void terminal_initialize(void);
+
+void terminal_enable_cursor(uint8_t cursor_start, uint8_t cursor_end);
+void terminal_disable_cursor(void);
+uint16_t terminal_get_cursor_position(void);
+void terminal_move_cursor(int x, int y);
+
 void terminal_putchar(char c);
 void terminal_write(const char* data, size_t size);
 void terminal_writestring(const char* data);
